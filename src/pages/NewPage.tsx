f3880307d022a94b11de71d2a5ff85df618043c8
MAG <mag_91@hotmail.de>
2022-08-02 15:49:25 +0200

    test

---
+++ src/pages/NewPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import MetaTags from "react-meta-tags";
+import { Container } from "reactstrap";
+
+//Import Breadcrumb
+import Breadcrumbs from "../components/Common/Breadcrumb";
+
+    const NewPage = () => {
+        return (
+          console.log("Hello")
+        );
+    }
+
+export default NewPage;
